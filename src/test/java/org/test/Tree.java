820b6cc5b9b972b22afae76849122419089ac817
ajaymani <ajaymani311@gmail.com>
2023-09-17 19:35:42 +0530

    Pro

---
+++ src/test/java/org/test/Tree.java
@@ -0,0 +1,13 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class Tree {
+	@Test 
+	public void test() {
+		System.out.println("Test 11");
+		System.out.println("Test 17");
+		System.out.println("erty 23");
+	}
+
+}
