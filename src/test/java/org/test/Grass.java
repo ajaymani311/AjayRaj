a305c9fb8341c83fb477dff8fd607726139710f5
ajaymani <ajaymani311@gmail.com>
2023-09-17 18:54:32 +0530

    Ajay Code

--- src/test/java/org/test/Grass.java
+++ src/test/java/org/test/Grass.java
@@ -1,5 +1,12 @@
 package org.test;
 
+import org.testng.annotations.Test;
+
 public class Grass {
+	@Test
+	public void test1() {
+		System.out.println("Test 1");
+		System.out.println("Test 4");
+	}
 
 }
